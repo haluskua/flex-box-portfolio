b6fa17e22d4ba9dda6ac3bf2bb5da60d36dcdd40
haluskua <haluskua.one@gmail.com>
2021-03-17 16:59:31 +1300

    added hamburger menu cross with rotate and translate properties added in CSS

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const hamburderContainer = document.querySelector('#header__main-nav');
+const hamburgerContainer = document.querySelector('#header__main-nav');
 const hamburger = document.querySelector('.header__main-nav--hamburger');
 const links = document.querySelectorAll('.haeader__main-nav--links li ');
 
 // ad even listener
 
 hamburger.addEventListener('click', () => {
-    hamburderContainer.classList.toggle('clicked');
+    hamburgerContainer.classList.toggle('clicked');
 });
