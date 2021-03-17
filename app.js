1479bf0312236048d26660fadbb8a5b5e9e05518
haluskua <haluskua.one@gmail.com>
2021-03-17 22:53:29 +1300

    added fadded classlist to mobile interaction with hamburger menu plu added javascript funtionality

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const hamburgerContainer = document.querySelector('#header__main-nav');
 const hamburger = document.querySelector('.header__main-nav--hamburger');
-const links = document.querySelectorAll('.haeader__main-nav--links li ');
+const links = document.querySelectorAll('.header__main-nav--links li ');
 
 // ad even listener
 
 hamburger.addEventListener('click', () => {
     hamburgerContainer.classList.toggle('clicked');
+    links.forEach((link) => {
+        link.classList.toggle("fade");
+    });
 });
